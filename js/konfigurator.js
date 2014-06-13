6c65caa68350b5b1ff67569733db71670dad7db4
Tim Zimmermann <tz@spreadyourmind.de>
2014-06-13 22:56:39 +0200

    This Works!

---
+++ js/konfigurator.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+					/*Austausch der Imagebilder*/
+					
+					
+					
+					/*Variablen definieren */
+					
+					/* 
+					var eiche
+					var obj = $("img", "#konfigurator");
+
+					
+					obj.click(function(){
+							var pos = $(this).attr("src");
+							var posTarget = $(this);
+							var baseImg = $(obj.get(0) );
+							var temp = baseImg.attr("src");
+
+
+							baseImg.fadeOut('slow', function() {
+								baseImg.attr("src", pos)
+								.fadeIn('slow');
+							});
+						
+							posTarget.fadeOut('slow', function() {
+								posTarget.attr("src", temp)
+								.fadeIn('slow');
+							});
+						
+					});
+
+
+
+}); // document.ready schließen
+
+				
+			
